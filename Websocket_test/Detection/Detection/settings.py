912c43d8fed3ff2bbed3e6391c40026f3a6fccd7
SaiDhinakar <saidhinakars899@gmail.com>
2024-11-27 11:56:47 +0530

    code clean up

--- Websocket_test/Detection/Detection/settings.py
+++ Websocket_test/Detection/Detection/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'web/static')
 
 
 # Default primary key field type
--- Websocket_test/Detection/web/urls.py
+++ Websocket_test/Detection/web/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 urlpatterns = [
     path('show_data/', views.my_view),
-    path('',views.home),
+    path('',views.home,name='home'),
     path('detections/',views.detection_view,name='detection'),
     path('camera_id/',views.camera_id,name='camera_id'),
-    path('get-data/', views.get_data, name='get_data'),
+    path('api/cameras/', views.get_data, name='camera_data'),
     path('save-data/', views.save_data_view, name='save_data'),
     path('delete-data/<int:index>/', views.delete_data, name='delete_data'),
-    # path('video_feed/<int:camera_id>/', views.video_feed, name='video_feed'),
-    # path('live_stream/<int:idx>',views.live_stream, name="live_stream"),
 ]   
