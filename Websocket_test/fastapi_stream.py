0027ee2f643ab2d5589371e5de58ec27b4b4db55
Drackko <73512940+Drackko@users.noreply.github.com>
2024-12-02 12:31:50 +0530

    Face rec model upgrade and detection logic fix

--- Websocket_test/fastapi_stream.py
+++ Websocket_test/fastapi_stream.py
@@ -10,7 +10,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from datetime import datetime
 from pathlib import Path
-from Face_recognition.face_recognize_yolo_updated import recognize_faces_in_persons
+from Face_recognition.face_recognize_yolo import recognize_faces_in_persons
+from Face_recognition.FaceTracker import FaceTracker
 from ID_detection.yolov11.ID_Detection import detect_id_card
 from Detection.Detection.settings import STATIC_ROOT
 import time
@@ -21,8 +22,7 @@ app = FastAPI()
 
 # MongoDB connection setup
 try:
-    client = MongoClient(
-        os.getenv("MONGO_URI", 'mongodb+srv://ml_dept_project:ml_dept_project@ml-project.gkigx.mongodb.net/'))
+    client = MongoClient(os.getenv("MONGO_URI", 'mongodb+srv://ml_dept_project:ml_dept_project@ml-project.gkigx.mongodb.net/'))
     db = client['ML_project']
     collection = db['DatabaseDB']
 except Exception as e:
@@ -32,7 +32,6 @@ except Exception as e:
 # Load camera data from data.json
 DATA_FILE_PATH = Path('./Detection/data.json')
 cached_data = None
-
 
 def load_data():
     global cached_data
@@ -49,43 +48,64 @@ def load_data():
             cached_data = []
     return cached_data
 
-
 # Load camera data
 # camera_data = [{'camera_ip': 0,'camera_location':'lh_32'}]  # Adjust for your actual camera IP or path
 camera_data = load_data()
 print(camera_data)
 # A dictionary to store frames for each camera
 current_frames = {}
-
-
-# Function to capture frames from each camera
+camera_trackers = {}# Function to capture frames from each camera
 def capture_frame(camera_index, camera_ip):
+    global camera_trackers
+    if camera_index not in camera_trackers:
+        camera_trackers[camera_index] = FaceTracker()
+
+    face_tracker = camera_trackers[camera_index]
     cap = cv2.VideoCapture(camera_ip)  # RTSP stream URL
+
     if not cap.isOpened():
         print(f"Failed to open camera {camera_index} at {camera_ip}")
         return
-
+    last_save_time = time.time()
+    save_interval = 2.0  # Save interval in seconds
+    frame_count = 0
     while True:
         ret, frame = cap.read()
         if ret:
+            frame_count += 1
+
+            if frame_count % 1 == 0:
+                # Process the frame for face and ID detection
+                modified_frame, person_boxes, associations = detect_id_card(frame)
+                modified_frame, flags  = recognize_faces_in_persons(modified_frame, person_boxes,face_tracker)
+                # print(camera_data, camera_id)
+                if time.time() - last_save_time > save_interval:
+                    location = camera_data[camera_index]['camera_location']
+                    # Save detections to MongoDB based on conditions
+                    process_and_save_detections(
+                        frame=frame,
+                        person_bboxes=person_boxes,
+                        flags=flags,
+                        associations=associations,
+                        camera_location=location
+                    )
+
             # Encode the frame to JPEG and then base64
             _, jpeg = cv2.imencode('.jpg', frame)
             current_frames[camera_index] = base64.b64encode(jpeg.tobytes()).decode('utf-8')
         else:
             print(f"Error capturing frame from camera {camera_index}")
 
-
 # Start a separate thread for each camera to capture frames
 for index, camera in enumerate(camera_data):
     threading.Thread(target=capture_frame, args=(index, camera["camera_ip"]), daemon=True).start()
-
 
 # Function to process and save detections to MongoDB
 def process_and_save_detections(frame, person_bboxes, flags, associations, camera_location):
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     for idx, (person_box, flag) in enumerate(zip(person_bboxes, flags)):
-        if flag == "UNDETERMINED":  # Skip if no face detected
+        if flag == "PENDING":  # Skip if no face detected
             continue
 
         # Extract person sub-image from frame
@@ -108,8 +128,8 @@ def process_and_save_detections(frame, person_bboxes, flags, associations, camer
                 document = {
                     "_id": ObjectId(),
                     "location": camera_location,
-                    "time": datetime.now().strftime("%D %I:%M %p"),
-                    "Role": "Unidentified" if flag == "UNKNOWN" else "Insider",
+                    "time": datetime.now().strftime("%D %I:%M %p") ,
+                    "Role": "Unidentified" if flag == "UNKNOWN" else "SIETIAN",
                     "Wearing_id_card": wearing_id_card,
                     "image": "/images/" + image_name,
                 }
@@ -120,44 +140,23 @@ def process_and_save_detections(frame, person_bboxes, flags, associations, camer
             except Exception as e:
                 print(f"Error saving detection data to database: {e}")
 
-
 # FastAPI WebSocket endpoint for video feed
 @app.websocket("/ws/video/{camera_id}/")
 async def video_feed(websocket: WebSocket, camera_id: int):
     await websocket.accept()
 
-    last_save_time = time.time()
-    save_interval = 2.0  # Save interval in seconds
-    frame_count = 0
+
     while True:
         if camera_id in current_frames:
             # Decode the current frame from base64 to an image
             frame_data = base64.b64decode(current_frames[camera_id])
             frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-            frame_count += 1
-
-            if frame_count % 3 == 0:
-                # Process the frame for face and ID detection
-                modified_frame, person_boxes, associations = detect_id_card(frame)
-                modified_frame, flags = recognize_faces_in_persons(modified_frame, person_boxes)
-
-                # print(camera_data, camera_id)
-                if time.time() - last_save_time > save_interval:
-                    location = camera_data[camera_id]['camera_location']
-                    # Save detections to MongoDB based on conditions
-                    process_and_save_detections(
-                        frame=frame,
-                        person_bboxes=person_boxes,
-                        flags=flags,
-                        associations=associations,
-                        camera_location=location
-                    )
 
                     # Send the modified frame to the client as base64
-                    _, jpeg = cv2.imencode('.jpg', modified_frame)
-                    frame_b64 = base64.b64encode(jpeg.tobytes()).decode('utf-8')
-                    await websocket.send_text(f'{{"frame": "{frame_b64}"}}')
+            _, jpeg = cv2.imencode('.jpg', frame)
+            frame_b64 = base64.b64encode(jpeg.tobytes()).decode('utf-8')
+            await websocket.send_text(f'{{"frame": "{frame_b64}"}}')
 
             await asyncio.sleep(0.1)  # Sleep to prevent excessive CPU usage
 
@@ -165,6 +164,5 @@ async def video_feed(websocket: WebSocket, camera_id: int):
 if __name__ == "__main__":
     # Run the FastAPI server
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=7000)
 
