2e00303834dc9ae3b0bb6d168e3bd6c4fcfb22ae
SaiDhinakar <saidhinakars899@gmail.com>
2024-12-02 12:08:30 +0530

    settings views s.no updated in detection.html

--- Websocket_test/Detection/Detection/settings.py
+++ Websocket_test/Detection/Detection/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web',
-    # 'djongo',
-    # 'channels',
+    'web'
 ]
 
 MIDDLEWARE = [
